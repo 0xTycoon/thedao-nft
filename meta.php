00e1ca93dcd6c0c5f1c960796af5715b5006da46
0xTycoon <0xTycoon@users.noreply.github.com>
2021-11-16 16:47:49 +0900

    update metadata description and generation

--- meta.php
+++ meta.php
@@ -28,15 +28,23 @@ for ($i = 0; $i < 18; $i++) {
         }
         $pageID = $i+1;
         $tileID = $t+1;
-        $json = <<<END
-{
-  "name": "TheNFT #$serial/1799",
-  "description": "The SEC issued an 18-page investigative report about TheDAO. The report concluded that, DAO Tokens, a Digital Asset, were securities. Each page of the pfd has been converted into bitmaps, then broken up into 100 tiles. Each tile is minted as an NFT, with 1 original TheDAO token wrapped inside. This is page $pageID, tile $tileID (serial numbers start from 0)",
-  "image": "ipfs://QmRX4tA5VtPPvetDdTx9SD9Wm1ty4zzeqHzq4y9guvUHAk/$i/$t.png",
-  "attributes": []
-}
-END;
-        file_put_contents("$path_mata/$t.json", $json);
+
+        $obj = new stdClass;
+        $obj->name = "TheNFT #$serial/1799";
+        $obj->description = "The SEC issued an 18-page investigative report about TheDAO. The report concluded that, DAO Tokens, a Digital Asset, were securities. Each page of the pdf has been converted into bitmaps, then broken up into 100 tiles.
+
+Each tile is minted as an NFT, with 1 original TheDAO token wrapped inside.
+
+The NFT can be burned to unwrap 1 TheDAO token, which is a 16 decimal ERC20 token, created on Ethereum on Apr 30 2016.
+
+This piece is from page $pageID, tile $tileID (serial numbers start from 0)
+
+More details: thedaonft.eth
+  ";
+        $obj->image = "ipfs://QmRX4tA5VtPPvetDdTx9SD9Wm1ty4zzeqHzq4y9guvUHAk/$i/$t.png";
+        $obj->attributes = [];
+
+        file_put_contents("$path_mata/$t.json", json_encode($obj, JSON_PRETTY_PRINT));
 
     }
 }
